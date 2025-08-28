cfa8b8f5ac70fe5a9d9150766ed30a01a09f9510
Ramiro <ramiro20010202@gmail.com>
2025-08-28 12:04:54 -0600

    feat(ui): add UserController and user management index page

--- farmacia/src/main/java/com/mycompany/controller/UserController.java
+++ farmacia/src/main/java/com/mycompany/controller/UserController.java
@@ -4,10 +4,57 @@
  */
 package com.mycompany.controller;
 
+import com.mycompany.model.entity.User;
+import com.mycompany.service.IUserService;
+import jakarta.ejb.EJB;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+import lombok.Data;
+import org.primefaces.PrimeFaces;
+
 /**
  *
  * @author ramir
  */
-public class UserController {
+@Data
+@Named(value = "userMB")
+@ViewScoped
+public class UserController implements Serializable{
+    @EJB
+    private IUserService userService;
+    private User user;
+    private List<User> users;  
     
+    public void createNew(){
+        user = new User();
+    }    
+    
+    public List<User> getUsers(){
+        return users = userService.list();
+    }   
+    
+    public void save(){
+        if(user.getId() == null){
+            userService.save(user);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("User added"));
+        }
+        else{
+            userService.edit(user);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("User edited"));
+        }
+        createNew();
+        PrimeFaces.current().executeScript("PF('dlgUserRegister').hide()");
+        PrimeFaces.current().ajax().update("form:messages","form:dt-users");
+    }    
+    
+    public void delete(){
+        userService.delete(user);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("User deleted"));
+        PrimeFaces.current().executeScript("PF('dlgDeleteUser').hide()");
+        PrimeFaces.current().ajax().update("form:messages","form:dt-users");        
+    }    
 }
