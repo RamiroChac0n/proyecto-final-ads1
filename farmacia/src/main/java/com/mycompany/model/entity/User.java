544b17f082640b53e6bf0bfc2e6445709e81add1
Ramiro <ramiro20010202@gmail.com>
2025-08-28 12:07:18 -0600

    feat(entity): add JPA persistence annotations to User entity

--- farmacia/src/main/java/com/mycompany/model/entity/User.java
+++ farmacia/src/main/java/com/mycompany/model/entity/User.java
@@ -4,6 +4,15 @@
  */
 package com.mycompany.model.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 /**
@@ -13,11 +22,36 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+@Entity
+@Table(name="users")
 public class User {
+    
+    @Id
+    @Column(name = "id", length = 13)
     private String id;
+    
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "name", nullable = false, length = 100)
     private String name;
+    
+    @Size(max = 8)
+    @Column(name = "phone_number", length = 8)
     private String phoneNumber;
+    
+    @Email
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "email", nullable = false, unique = true, length = 100)
     private String email;
+    
+    @NotBlank
+    @Size(max = 255)
+    @Column(name = "password", nullable = false, length = 255)
     private String password;
+    
+    
+    @Column(name = "role")    
     private String role;
 }
